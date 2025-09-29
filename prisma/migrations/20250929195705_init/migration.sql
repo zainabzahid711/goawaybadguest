1c4ac2dfae0fa93c80e698aac42dfffe073e0f5c
zainabzahid711 <zainabzahid711@gmail.com>
2025-09-30 01:05:17 +0500

    - initialized prisma prisma the ORM (Object Relational Mapper) and argon2 A password hashing algorithm.
    - User table exists in Neon Postgres.
    - Prisma is connected to your DB via .env.

---
+++ prisma/migrations/20250929195705_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
